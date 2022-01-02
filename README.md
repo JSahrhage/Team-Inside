e5794a2206bb463be9ba81bdebe70a1c14ed9993
Jan <jan-sahrhage@gmx.de>
2022-01-02 16:56:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Team Inside
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // configureInjection(Environment.prod);
+  // await Firebase.initializeApp();
+  // runApp(AppWidget());
+}
